474ee30685663000baf6d93175e3914668bb9454
ErosAlx <erosalx1@gmail.com>
2023-09-07 15:39:48 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,17 @@
-#include <unistd.h>
+#include<unistd.h>
+
 /**
  * main - Entry point
  *
- * Description: C program that prints quote using write function
- * size_t write(int fd, const void *buf, size_t count);
+ * Description: C program that print a quote using write function
  *
- * Return: This time we return an error 1.
+ * Return: return an error (1)
 */
+
 int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1,quo,59);
+
+	write(1, quo, 59);
 	return (1);
 }
