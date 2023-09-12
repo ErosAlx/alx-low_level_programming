42f45403ce8a4cf5a94d47d6d8204cec5fc07472
ErosAlx <erosalx1@gmail.com>
2023-09-12 16:18:48 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if character is a letter.
+ *
+ * @c: takes input from other functions.
+ *
+ * Return: return 1 if c is a letter else 0.
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') && (c >= 'a' && c <= 'z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
