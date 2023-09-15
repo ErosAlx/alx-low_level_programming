1f9587eca4cce5cbae22ebaa9532baa0c6538bdc
ErosAlx <erosalx1@gmail.com>
2023-09-14 17:36:11 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -3,21 +3,27 @@
 /**
  * print_square - printing square using #
  *
- * @size: size of the square
+ * @size: size of the square.
  *
- * Return: return 0
-*/
-
+ * Return: return 0.
+ */
 void print_square(int size)
 {
 	int i, x;
 
-	for (i = 1; i <= size; i++)
+	if (size > 0)
 	{
-		for (x = 1; x <= size; x++)
+		for (i = 0; i < size; i++)
 		{
-			_putchar('#');
+			for (x = 0; x < size; x++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
+	}
+	else
+	{
 		_putchar('\n');
 	}
 }
