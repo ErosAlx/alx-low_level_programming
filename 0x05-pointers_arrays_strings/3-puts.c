7d2928e52f3715fc653756810331412f2da90514
ErosAlx <erosalx1@gmail.com>
2023-09-18 13:34:10 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - function printing a string followed
+ * by a new line to stdout.
+ *
+ * @str: string takes input from other function.
+ *
+ * Return: return str.
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		str++;
+	}
+	_putchar('\n');
+}
