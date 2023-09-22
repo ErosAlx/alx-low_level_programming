f21de8aea1a319bb26a6160f97f068092114d5dd
ErosAlx <erosalx1@gmail.com>
2023-09-21 19:32:44 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,5 +11,7 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *str);
+char *rot13(char *);
+void print_number(int n);
 
 #endif /* MAIN_H */
