690bf03c45cf907995c5e6332a11a9181f12af98
ErosAlx <erosalx1@gmail.com>
2023-10-10 12:52:55 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,16 +5,16 @@
 
 /**
  * struct dog - a dog struct
- *
  * @name: dog's name
  * @age: dog's age
  * @owner: owner's name
  */
+
 struct dog
 {
-	char* name;
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
 
 #endif /* DOG_H */
