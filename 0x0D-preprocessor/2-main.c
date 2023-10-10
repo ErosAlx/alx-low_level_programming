4d5a20d18f9e4889c9664c08a726f3ad397a894b
ErosAlx <erosalx1@gmail.com>
2023-10-10 09:49:23 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: program that prints the name of the file it was compiled from.
+ *
+ * Return: returns 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
