9b1a8013a5817c2726b6ddf45d5030671fc02ce1
ErosAlx <erosalx1@gmail.com>
2023-10-11 15:22:23 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -17,7 +17,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array != NULL && size > 0 && action != NULL)
 	{
-		for (i = 0; index < size; i++)
+		for (i = 0; i < size; i++)
 		{
 			action(array[i]);
 		}
