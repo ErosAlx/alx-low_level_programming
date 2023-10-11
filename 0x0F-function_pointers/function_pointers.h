aaa849f3160989941e9765e7775e1f9d9f5cdc1e
ErosAlx <erosalx1@gmail.com>
2023-10-11 15:48:48 -0700

    3-

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h> /*for atoi*/
+#include <stdlib.h> /*for exit*/
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
