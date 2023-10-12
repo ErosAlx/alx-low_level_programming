3963d968f1790643a0f211089a5671a7b028a770
ErosAlx <erosalx1@gmail.com>
2023-10-12 15:42:58 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -6,13 +6,14 @@
 * @arg: A list of arguments pointing to
 * the character to be printed.
 */
+
 void print_char(va_list arg)
 {
-char letter;
+	char letter;
 
-letter = va_arg(arg, int);
+	letter = va_arg(arg, int);
 
-printf("%c", letter);
+	printf("%c", letter);
 }
 
 /**
@@ -20,50 +21,49 @@ printf("%c", letter);
 * @arg: A list of arguments pointing to
 * the integer to be printed.
 */
+
 void print_int(va_list arg)
 {
-int num;
+	int num;
 
-num = va_arg(arg, int);
+	num = va_arg(arg, int);
 
-printf("%d", num);
+	printf("%d", num);
 }
-
 
 /**
 * print_float - Prints a float.
 * @arg: A list of arguments pointing to
 * the float to be printed.
 */
+
 void print_float(va_list arg)
 {
-float num;
+	float num;
 
-num = va_arg(arg, double);
+	num = va_arg(arg, double);
 
-printf("%f", num);
+	printf("%f", num);
 }
-
-
 
 /**
 * print_string - Prints a string.
 * @arg: A list of arguments pointing to
 * the string to be printed.
 */
+
 void print_string(va_list arg)
 {
-char *str;
+	char *str;
 
-str = va_arg(arg, char *);
+	str = va_arg(arg, char *);
 
-if (str == NULL)
-{
-printf("(nil)");
-return;
-}
-
-printf("%s", str);
+	if (str == NULL)
+	{
+	printf("(nil)");
+	return;
+	}
+	printf("%s", str);
 }
 
 /**
@@ -72,13 +72,12 @@ printf("%s", str);
 *
 *Return: nothing
 */
+
 void print_all(const char * const format, ...)
 {
-va_list args;
-
-int i = 0, j = 0;
-
-char *separator = "";
+	va_list args;
+	int i = 0, j = 0;
+	char *separator = "";
 
 printer_t funcs[] = {
 {"c", print_char},
@@ -87,27 +86,23 @@ printer_t funcs[] = {
 {"s", print_string}
 };
 
-va_start(args, format);
+	va_start(args, format);
 
-while (format && (*(format + i)))
-{
-j = 0;
+	while (format && (*(format + i)))
+	{
+	j = 0;
+	
+	while (j < 4 && (*(format + i) != *(funcs[j].symbol)))
+	j++;
 
-while (j < 4 && (*(format + i) != *(funcs[j].symbol)))
-j++;
-
-if (j < 4)
-{
-printf("%s", separator);
-funcs[j].print(args);
-separator = ", ";
-}
-
-i++;
-
-}
-
-printf("\n");
-
-va_end(args);
+	if (j < 4)
+	{
+		printf("%s", separator);
+		funcs[j].print(args);
+		separator = ", ";
+	}
+	i++;
+	}
+	printf("\n");
+	va_end(args);
 }
