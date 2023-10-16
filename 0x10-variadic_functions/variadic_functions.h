1f41723214213c154ef291bd6798e92407edd257
ErosAlx <erosalx1@gmail.com>
2023-10-15 21:25:06 -0700

    variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,6 +9,21 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * struct printer - Struct format_types
+ * @symbol: The conversion specifier
+ * @print: The function pointer
+ */
 
+typedef struct printer
+{
+char *symbol;
+void (*print)(va_list arg);
+} printer_t;
+
+void print_char(va_list arg);
+void print_int(va_list arg);
+void print_float(va_list arg);
+void print_string(va_list arg);
 
 #endif /* VARIADIC_FUNCTIONS_H */
