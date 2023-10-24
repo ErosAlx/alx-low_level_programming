7d65076c232377d73cfa7ebe61d91725986b3a66
ErosAlx <erosalx1@gmail.com>
2023-10-24 02:36:42 -0700

    2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,9 +12,12 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new_node;
+	listint_t *new_node = malloc(sizeof(listint_t));
 
-	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+	{
+		return (0);
+	}
 
 	new_node->n = n;
 	new_node->next = *head;
