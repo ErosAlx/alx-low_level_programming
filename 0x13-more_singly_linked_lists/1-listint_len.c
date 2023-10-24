caf89fe7cb96516758c82b4c5f6e3f3d2429a625
ErosAlx <erosalx1@gmail.com>
2023-10-24 02:05:38 -0700

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * listint_len - a function that returns the number of elements.
+ *
+ * @h: pointer to the first node
+ *
+ * Return: number of elements
+*/
+
+size_t listint_len(const listint_t *h)
+{
+	size_t n_elements = 1;
+
+	if (h == NULL)
+		return (0);
+
+	while (h->next != NULL)
+	{
+		if (h->n != NULL)
+		{
+			h = h->next;
+			n_elements++;
+		}
+	}
+	return (elements);
+}
