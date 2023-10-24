a8decea3b0f287b0bfdc94fe9b18b3f2a63be1ca
ErosAlx <erosalx1@gmail.com>
2023-10-24 06:03:03 -0700

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -10,10 +10,11 @@
 
 void free_listint(listint_t *head)
 {
-	listint_t *current = head;
+	listint_t *current;
 
 	while (head != NULL)
 	{
+		current = head;
 		head = head->next;
 		free(current);
 	}
