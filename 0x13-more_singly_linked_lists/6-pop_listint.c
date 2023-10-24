4f04695782518e37ad9947b17ed20c67d1993a04
ErosAlx <erosalx1@gmail.com>
2023-10-24 06:58:50 -0700

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,10 +1,26 @@
 #include "lists.h"
 
+/**
+ * pop_listint - deletes head of the node
+ *
+ * @head: pointer to the first node in the list
+ *
+ * Return: the head's node's data (n)
+*/
+
 int pop_listint(listint_t **head)
 {
-	if(head == NULL)
-	{
+	int firstnode_data;
+	listint_t *temp, *next;
+
+	/*if list is empty return 0*/
+	if (*head == NULL)
 		return (0);
-	}
 
-
+	temp = *head;
+	next = temp->next;
+	firstnode_data = temp->n;
+	free(temp);
+	*head = next;
+	return (firstnode_data);
+}
