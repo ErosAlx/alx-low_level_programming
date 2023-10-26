7771207d8468589aa4972de25cd0bf78993dafb0
ErosAlx <erosalx1@gmail.com>
2023-10-26 14:58:02 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_binary - prints a number as binary string
+ * @n: number to print
+ *
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	int bit = 8 * sizeof(n), printed = 0;
+
+	while (bit != '\0')
+	{
+		if (n & 1L << --bit)
+		{
+			_putchar('1');
+			printed++;
+		}
+		else if (printed != '\0')
+		{
+			_putchar('0');
+		}
+	}
+	if (printed == '\0')
+		_putchar('0');
+}
